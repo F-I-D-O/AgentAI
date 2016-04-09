c1abb08eb19ca01fff630d83f48d13e810869c07
F.I.D.O. <david.fido.fiedler@gmail.com>
2016-04-09 17:51:11 +0200

    changes in doc

--- src/main/java/ninja/fido/agentai/Tools.java
+++ src/main/java/ninja/fido/agentai/Tools.java
@@ -12,13 +12,13 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * This class contains static helper methods that don't belog to any specific part of the framework
+ * This class contains static helper methods that don't belog to any specific part of the framework.
  * @author F.I.D.O.
  */
 public class Tools {
 	
 	/**
-	 * Increment map entry by 1
+	 * Increment map entry by 1.
 	 * @param <K> map key type
 	 * @param map map to be changed
 	 * @param key key pointing to the value to be incremented
@@ -28,11 +28,11 @@ public class Tools {
 	}
 	
 	/**
-	 * Increment map entry by specified value
+	 * Increment map entry by specified value.
 	 * @param <K> map key type
 	 * @param map map to be changed
 	 * @param key key pointing to the value to be incremented
-	 * @param increment number the value will be incremented by
+	 * @param increment the number by which the value will be increased
 	 */
 	public static <K> void incrementMapValue(final Map<K,Integer> map, final K key, final int increment){
 		if(map.containsKey(key)){
@@ -44,7 +44,7 @@ public class Tools {
 	}
 	
 	/**
-	 * Sort map by value value
+	 * Sort map by value.
 	 * @param <K> key type
 	 * @param <V> value type
 	 * @param map map to be sorted
