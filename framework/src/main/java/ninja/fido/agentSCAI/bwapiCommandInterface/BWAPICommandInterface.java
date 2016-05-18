1fc29cdc4a2ce023868ab4c3ab82fc73b9ad7447
F.I.D.O <david.fido.fiedler@gmail.com>
2016-05-18 14:27:55 +0200

    command interface javadoc

--- framework/src/main/java/ninja/fido/agentSCAI/bwapiCommandInterface/BWAPICommandInterface.java
+++ framework/src/main/java/ninja/fido/agentSCAI/bwapiCommandInterface/BWAPICommandInterface.java
@@ -11,18 +11,42 @@ import ninja.fido.agentSCAI.agent.unit.Worker;
 import ninja.fido.agentSCAI.base.GameAgent;
 
 /**
- *
+ * Interface for BWAPI commands.
  * @author F.I.D.O.
  */
 public interface BWAPICommandInterface {
-		
+	
+	/**
+	 * Build coomand.
+	 * @param worker Worker that will construct the building.
+	 * @param buildingType Building type.
+	 * @param placeToBuildOn Place to build on.
+	 */
 	public void build(Worker worker, UnitType buildingType, TilePosition placeToBuildOn);
 	
+	/**
+	 * Attack move command.
+	 * @param agent Attacking agent.
+	 * @param target Attack move target.
+	 */
 	public void attackMove(UnitAgent agent, Position target);
 	
+	/**
+	 * Train command.
+	 * @param agent Game agent that will train new unit.
+	 * @param unitType Typpe of the unit that will be trained.
+	 */
 	public void train(GameAgent agent, UnitType unitType);
 	
+	/**
+	 * Move command.
+	 * @param agent Moving agent.
+	 * @param target Target position.
+	 */
 	public void move(UnitAgent agent, Position target);
 	
+	/**
+	 * This method should process commands queued from revious frame.
+	 */
 	public void processQueuedCommands();
 }
--- framework/src/main/java/ninja/fido/agentSCAI/bwapiCommandInterface/BwapiCommad.java
+++ framework/src/main/java/ninja/fido/agentSCAI/bwapiCommandInterface/BwapiCommad.java
@@ -6,24 +6,40 @@ package ninja.fido.agentSCAI.bwapiCommandInterface;
 import ninja.fido.agentSCAI.base.GameAgent;
 
 /**
- *
+ * BWAPI command wrapper.
  * @author F.I.D.O.
  */
 abstract class BwapiCommad<A extends GameAgent> {
+	
+	/**
+	 * Game agent that is affected by the command.
+	 */
 	private final A agent;
 
+	/**
+	 * Returns the target of the BWAPI command.
+	 * @return Returns the target of the BWAPI command.
+	 */
 	public A getAgent() {
 		return agent;
 	}
 	
 	
 	
-
+	/**
+	 * Constructor.
+	 * @param agent 
+	 */
 	public BwapiCommad(A agent) {
 		this.agent = agent;
 	}
 	
 	
 	
+	
+	/**
+	 * 
+	 * @return 
+	 */
 	public abstract String getType();
 }
